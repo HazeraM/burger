9d62b953249aef37b68aaa35cb66b1c508954ea4
Hazera Munim <hazeramunim@dyn-160-39-7-244.dyn.columbia.edu>
2019-07-12 14:40:29 -0400

    added the heroku link

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Imposible Burger");
+INSERT INTO burgers (burger_name) VALUES ("Triple-Stacked Avocado");
+INSERT INTO burgers (burger_name) VALUES ("Doubel Whopper");
+INSERT INTO burgers (burger_name) VALUES ("South of the Burger");
+INSERT INTO burgers (burger_name) VALUES ("Wild Buffalo Chicken Burger");
